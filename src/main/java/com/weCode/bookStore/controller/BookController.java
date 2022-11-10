dd6cc0a5975edcac2798a6d7ce8725c0cef397f7
Paulina <paulinakozaczek@gmail.com>
2022-11-10 13:35:58 +0000

    swagger ui integration implementation

--- src/main/java/com/weCode/bookStore/controller/BookController.java
+++ src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -2,6 +2,10 @@ package com.weCode.bookStore.controller;
 
 import com.weCode.bookStore.dto.BookDto;
 import com.weCode.bookStore.service.BookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(value = "Book Api", tags = "Book Api", produces = "application/json")
 @RestController
 @RequestMapping("api/v1/books")
 public class BookController {
@@ -17,6 +22,12 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    @ApiOperation(value = "get list of books", response = BookDto[].class, produces = "application/json")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of books"),
+            @ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(responseCode = "404", description = "Did not find resource")
+    })
     @GetMapping
     public ResponseEntity<List<BookDto>> getBooks() {
         List<BookDto> books = bookService.getBooks();
