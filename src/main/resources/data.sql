ee09a2dd1e08d0338d5c3f7a6afd78250b08dd7f
Paulina <paulinakozaczek@gmail.com>
2022-11-10 16:14:30 +0000

    build fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
+DELETE FROM book;
 insert into book(id, title, description, release_year) values ('c54b69a0-6108-11ed-9b6a-0242ac120002', 'Ikigai', 'The Japanese secret to a long and happy life', 2018);
